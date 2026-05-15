4b3ac18516241ddea8221869c156bda47d58101f
smansfeldg <santimansfeld@gmail.com>
2026-05-15 02:38:28 -0300

    feat: add multilingual support and update CV content
    - Implemented internationalization (i18n) with Spanish as default and English as an option in astro.config.mjs.
    - Updated CV content in cv_english.json to reflect new personal details and work experience.
    - Refactored components to accept CV data as props, enabling dynamic rendering based on the selected language.
    - Enhanced UI with language toggle and theme switcher in Layout.astro.
    - Added WhatsApp icon for contact methods in Hero.astro.
    - Created a new English version of the index page for the portfolio.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://smansfeldg.github.io'
+  site: 'https://smansfeldg.github.io',
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+  }
 });
