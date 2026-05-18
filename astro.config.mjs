0cf3dda9f4044aa59ed84ae41614d7d690cb8722
smansfeldg <santimansfeld@gmail.com>
2026-05-18 15:43:43 -0300

    fix: ensure consistent trailing slashes in language redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://smansfeldg.github.io',
+  trailingSlash: 'always',
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
