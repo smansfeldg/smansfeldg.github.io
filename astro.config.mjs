a9c2f78eeb30f63ae95bb62da55a4634ae3ab404
smansfeldg <santimansfeld@gmail.com>
2026-05-18 16:32:51 -0300

    fix: set base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://smansfeldg.github.io',
+  base: '/',
   trailingSlash: 'always',
   i18n: {
     defaultLocale: "es",
